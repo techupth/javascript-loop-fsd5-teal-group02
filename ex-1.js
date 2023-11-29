d4a5fd59e3f7dae7c6445ed489437f307eb4d28f
Palm <palml1lax@gmail.com>
2023-11-29 20:50:34 +0700

    feat: finish ex1-6.

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,10 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
+let totalScore = [];
+
+for (let i = 0; i < scores.length; i++) {
+  totalScore[i] = scores[i] - 10;
+}
+
+console.log(totalScore);
